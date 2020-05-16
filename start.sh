968cb01379026832fcb6d61eab0f9cd4c51eae2c
aierui <aieruishi@gmail.com>
2020-05-16 21:32:21 +0800

    add config start.sh

---
+++ start.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+dirPath=$(cd `dirname $0`; pwd)
+
+
+# brew install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap mongodb/brew
+brew install wget python python3 tree macvim node youtube-dl tmux pandoc cmake rmtrash reattach-to-user-namespace mongodb-community redis golang httpie jq
+# nodejs install
+npm install -g hexo-cli typescript
+# python install
+pip3 install virtualenv scipy numpy pandas jupyter tensorflow scikit-learn matplotlib seaborn pillow
+
+# install oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# zsh link
+ln -sf ${dirPath}/.zshrc ~/.zshrc
+
+
+## Links config file
+
+# git config
+ln -sf ${dirPath}/git/.gitconfig ~/.gitconfig
+
+
+## vim https://github.com/BroQiang/vim-go-ide
+ln -sf ${dirPath}/.vimrc ~/.vimrc
+
