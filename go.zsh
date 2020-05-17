523edc3c7eb7872d9b18899b42f7a0d5a9970692
aierui <aieruishi@gmail.com>
2020-05-17 18:16:11 +0800

    update go bash variables

--- go.zsh
+++ go.zsh
@@ -4,7 +4,8 @@
 
 # export GOROOT="${DEV_PATH}/go-core-lib/1.13.0"
 # export GOROOT="${DEV_PATH}/go-core-lib/1.13.5"
-export GOROOT="${DEV_PATH}/go-core-lib/1.14.3"
+export GOVERSION="1.14.3"
+export GOROOT="${DEV_PATH}/go-core-lib/${GOVERSION}"
 export GOPATH="${DEV_PATH}/go-training"
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin:$GOBIN
