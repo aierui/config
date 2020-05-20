d51ffea44e24db4173432d7a9b7990c6c50ee666
aierui <aieruishi@gmail.com>
2020-05-20 14:19:05 +0800

    update start script file

--- start.sh
+++ start.sh
@@ -1,35 +1,97 @@
 #/bin/bash
 
-dirPath=$(cd `dirname $0`; pwd)
+dirPath=$(
+  cd $(dirname $0)
+  pwd
+)
 
+LOG_FILE=/tmp/aierui_config_start.log
 
-# brew install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap mongodb/brew
-brew install wget python python3 tree macvim node youtube-dl tmux pandoc cmake rmtrash reattach-to-user-namespace mongodb-community redis golang httpie jq
-# nodejs install
-npm install -g hexo-cli typescript
-# python install
-pip3 install virtualenv scipy numpy pandas jupyter tensorflow scikit-learn matplotlib seaborn pillow
+function log() {
+  echo ">> $(date) $*" >>${LOG_FILE}
+}
 
-# install zsh 
-# install oh-my-zsh (before install zsh)
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-# install zsh theme
-cd $ZSH/custom/plugins/ && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-cd $ZSH/custom/plugins/ && git clone https://github.com/zsh-users/zsh-autosuggestions.git
+function elog() {
+  echo "$*"
+  echo ">> $(date) $*" >>${LOG_FILE}
+}
 
-# zsh link
-ln -sf ${dirPath}/.zshrc ~/.zshrc
+function run() {
+  log "@$*"
+  # shellcheck disable=SC2068
+  $@ >>${LOG_FILE} 2>&1
+}
 
+function installDependentOnLinux() {
+  log "start install that dependent soft ware"
 
-## Links config file
+  run yum install -y
+  sysstat \
+    dstat \
+    iftop \
+    iotop \
+    telnet \
+    net-tools \
+    jq \
+    vim \
+    gcc \
+    gcc-c++ \
+    make \
+    ctags \
+    tree
 
-# git config
-ln -sf ${dirPath}/git/.gitconfig ~/.gitconfig
-ln -sf ${dirPath}/git/.gitignore_global ~/.gitignore_global
+  log "install finished that dependent soft ware"
+}
 
+function installDependentOnMac() {
+  # brew install
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew tap mongodb/brew
+  brew install wget python python3 tree node cmake vim jq
+  # nodejs install
+  npm install -g hexo-cli typescript
+}
 
-## vim https://github.com/BroQiang/vim-go-ide
-ln -sf ${dirPath}/.vimrc ~/.vimrc
+function installSoftWare() {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    installDependentOnMac
+  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    installDependentOnLinux
+  elif [[ "$OSTYPE" == "freebsd"* ]]; then
+    installDependentOnLinux
+  else
+    exit
+  fi
+}
 
+function installExtend() {
+  # install zsh
+  # install oh-my-zsh (before install zsh)
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+  # install zsh theme
+  cd $ZSH/custom/plugins/ && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+  cd $ZSH/custom/plugins/ && git clone https://github.com/zsh-users/zsh-autosuggestions.git
+}
+
+function linkFile() {
+  # zsh link
+  ln -sf ${dirPath}/.zshrc ~/.zshrc
+  ## Links config file
+  # git config
+  ln -sf ${dirPath}/git/.gitconfig ~/.gitconfig
+  ln -sf ${dirPath}/git/.gitignore_global ~/.gitignore_global
+  ## vim https://github.com/BroQiang/vim-go-ide
+  ln -sf ${dirPath}/.vimrc ~/.vimrc
+}
+
+function start() {
+  log "start configuration environmental"
+
+  installSoftWare
+  installExtend
+  linkFile
+
+  log "end of configuration environmental"
+}
+
+start
